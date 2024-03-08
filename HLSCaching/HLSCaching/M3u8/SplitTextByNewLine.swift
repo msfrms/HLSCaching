d6f8c0bd5013edfc1481c336a05bb7fd2b39acff
Mikhail Radaev <msfrms@gmail.com>
2024-03-08 18:04:02 +0300

    added split text by lines

---
+++ HLSCaching/HLSCaching/M3u8/SplitTextByNewLine.swift
@@ -0,0 +1,14 @@
+//
+//  SplitTextByNewLine.swift
+//  HLSCaching
+//
+//  Created by Mikhail Radaev on 08.03.2024.
+//
+
+import Foundation
+
+class SplitTextByNewLine: TextBase {
+    override func tokens() -> [String] {
+        super.content().components(separatedBy: "\n")
+    }
+}
